3f7b61230f5d97d2584cb908c5504b9d2e3b5f6e
Keith Richardson <keith@richardsonfam.com>
2020-01-19 15:00:58 -0500

    Pixy and Path Weaver

---
+++ PixyCam2/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,154 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
+import io.github.pseudoresonance.pixy2api.links.SPILink;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private final Pixy2 pixy = Pixy2.createInstance(new SPILink());
+
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    pixy.init(); // Initializes the camera and prepares to send/receive data
+    pixy.setLED(200, 30, 255); // Sets the RGB LED to purple
+  }
+
+  private Joystick main = new Joystick(0);
+  private JoystickButton button1 = new JoystickButton(main, 1);
+	private final int blockSignature = 1;
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    if (button1.get()) {
+      pixy.setLamp((byte) 1, (byte) 1); // Turns the LEDs on
+      
+    } else {
+      pixy.setLamp((byte) 0, (byte) 0); // Turns the LEDs on
+      
+    }
+
+    Block foundBall = getBiggestBlock();
+		if (foundBall == null) {
+      SmartDashboard.putBoolean("PowerCell (Sig1) Found", false);
+			return;
+		}
+		
+    SmartDashboard.putBoolean("PowerCell (Sig1) Found", true);
+		SmartDashboard.putNumber( "PowerCell (Sig1) X", foundBall.getX());
+    SmartDashboard.putNumber( "PowerCell (Sig1) Y", foundBall.getY());
+ }
+
+
+  public Block getBiggestBlock() {
+		// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
+		// does not wait for new data if none is available,
+		// and limits the number of returned blocks to 25, for a slight increase in efficiency
+		int blockCount = pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+		//System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
+		if (blockCount <= 0) {
+			return null; // If blocks were not found, stop processing
+		}
+		ArrayList<Block> blocks = pixy.getCCC().getBlocks(); // Gets a list of all blocks found by the Pixy2
+		Block largestBlock = null;
+		for (Block block : blocks) { // Loops through all blocks and finds the widest one
+			if (largestBlock == null) {
+				largestBlock = block;
+			} else if (block.getWidth() > largestBlock.getWidth()) {
+				largestBlock = block;
+			}
+		}
+		return largestBlock;
+	}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
+   *
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+  }
+}
